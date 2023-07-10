25f4393ede92bf5ae937330f7485ea6d28a650af
Md. Tamim khan <mdtamimkhan842@gmail.comgit config --global user.name mdtamimkhan842omit --global to set the identity only in the repository$ git config --global user.email mdtamimkhan842@gmail.com>
2023-07-11 02:09:35 +0600

    second commit

--- Highest Divisor.cpp
+++ Highest Divisor.cpp
@@ -19,12 +19,12 @@ int main()
     vector<int> v;
     for (int i = 1; i <= n; i++)
     {
-        if (n % i == 0)
+        if (n % i == 0) // 1 13 7 91
         {
             v.push_back(i);
         }
     }
-    int maxi = v[0];
+    int maxi = v[0]; // 1
     for (int i = 0; i < v.size(); i++)
     {
         if (v[i] < 11)
