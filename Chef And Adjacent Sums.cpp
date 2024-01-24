8595973babc968ded1d1f591696e222acaccd983
Tamim khan <mdtamimkhan842@gmail.com>
2024-01-25 02:17:52 +0600

    second commit

---
+++ Chef And Adjacent Sums.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ordered_set tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update>
+#define MTK                       \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+typedef long long int ll;
+#define all(x) x.begin(), x.end()
+#define mod 1000000007
+int32_t main()
+{
+    MTK;
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int a[n];
+        for (int i = 0; i < n; i++)
+            cin >> a[i];
+        // sort(a, a + n);
+        int cnt = 0;
+        int z = a[n - 1] + a[n - 2];
+        for (int i = 0; i < n - 2; i++)
+        {
+            if ((a[i] + a[i + 1]) < z)
+                cnt++;
+        }
+        if ((cnt == n - 2) and n > 2)
+            cout << "YES" << '\n';
+        else
+            cout << "NO" << '\n';
+    }
+}
